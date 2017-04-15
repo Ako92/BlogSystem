c822de068a5fa0253002670ee3bdc8466d10dfa6
alihk12@mail.com <alihk12@mail.com>
2017-04-15 15:45:37 +0430

    set BlogSystem urls.py

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ from django.urls import reverse
 
 class BlogAuthor(models.Model):
     user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
-    bio = models.CharField(max_length=200,help_text='Enter Your Biography')
+    bio = models.CharField(max_length=200, help_text='Enter Your Biography')
 
     def __str__(self):
         return 'Blog author name is %s' % self.name
@@ -19,30 +19,30 @@ class BlogAuthor(models.Model):
 
 class Blog(models.Model):
 
-    name = models.CharField(max_length=200,help_text='Enter The name of Blog.')
+    name = models.CharField(max_length=200, help_text='Enter The name of Blog.')
     description = models.TextField(max_length=2000, help_text='Enter your blog text here.')
-    author =  models.ForeignKey(BlogAuthor,on_delete=models.SET_NULL,null=True)
+    author = models.ForeignKey(BlogAuthor, on_delete=models.SET_NULL, null=True)
     post_date = models.DateField(default=date.today)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('blog-detail',args=str(self.id))
+        return reverse('blog-detail', args=str(self.id))
 
 
 class BlogComment(models.Model):
-    description = models.TextField(max_length=1000,help_text='Enter your Coment Here')
+    description = models.TextField(max_length=1000, help_text='Enter your Coment Here')
     post_date = models.DateTimeField(auto_now_add=date)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    blog = models.ForeignKey(Blog,on_delete=models.CASCADE)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['post_date']
 
     def __str__(self):
         len_title = 75
-        if len(self.description)>len_title:
+        if len(self.description) > len_title:
             title_string = self.description[:len_title]
         else:
             title_string = self.description
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Blog, BlogAuthor,BlogComment
 
 # Create your views here.
+def index(request):
+    pass
